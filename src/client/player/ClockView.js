d12d2cc27fae29d029c99afe57ef75959c771178
Diemo Schwarz <schwarz@ircam.fr>
2018-05-25 11:33:28 +0200

    switch back to 1-digit hour format that fits on phone screens, implement most options

--- src/client/player/ClockView.js
+++ src/client/player/ClockView.js
@@ -25,20 +25,22 @@ function padLeft(value, char, length) {
 }
 
 function formatTime(time) {   // [-][hh:]mm:ss
-    // possible options (really needed ? if not remove that):
-    // const hoursTwoDigits = false;   // always pad hour to two digits by prepending "0"
-    // const hoursAlways = false;       // todo
-    // const colonFlash  = false;       // todo: flash separating ':' every 0.5 s
-    // const colonSymbol = ':';         // todo
+    // time display options: (to be configurable somewhere)
+    const hoursDigits     = 1;		// pad hour to n digits by prepending "0"
+    const hoursAlways     = false;	// always show hours, even when 0
+    const separatorFlash  = false;	// todo: flash separating ':' every 0.5 s
+    const separator       = ':';	// symbol separating digits
 
     const sign = time < 0 ? '-' : '';
     const timeInSeconds = Math.abs(Math.floor(time));
     // const secFrac = Math.abs(t) - timeInSeconds;	// fractional seconds (not used)
-    const hours = padLeft(Math.floor(timeInSeconds / 3600), '0', 2);
+    const hours   = Math.floor(timeInSeconds / 3600);
+    const hourstr = padLeft(hours, '0', hoursDigits);
     const minutes = padLeft(Math.floor((timeInSeconds - (hours * 3600)) / 60), '0', 2);
-    const seconds = padLeft(timeInSeconds - (hours * 3600) - (minutes * 60), '0', 2);
+    const seconds = padLeft(timeInSeconds - (hours * 3600) - (minutes * 60),   '0', 2);
 
-    return sign + (hours !== '00' ? hours + ':' : '') + minutes + ':' + seconds;
+    return sign + (hours !== 0  ||  hoursAlways  ?  hourstr + separator  :  '')
+	   + minutes + separator + seconds;
 }
 
 class ClockView extends soundworks.SegmentedView {
